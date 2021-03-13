ab6c6d16b39656ed9c6c8ef2f9cb8ca30b298b40
Frank Nguyen <frank.nguyen.vd@gmail.com>
2021-03-13 15:02:51 +0800

    Add missing genres to Venue model and missing representation string

--- app.py
+++ app.py
@@ -39,10 +39,14 @@ class Venue(db.Model):
     state = db.Column(db.String(120))
     address = db.Column(db.String(120))
     phone = db.Column(db.String(120))
+    genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
 
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    # DONE: implement any missing fields, as a database migration using Flask-Migrate
+    def __repr__(self):
+      return f'<Venue: {self.id}, {self.name}, {self.city}, {self.state}, {self.address}, {self.phone}, {self.genres}, {self.image_link}, {self.facebook_link}>'
+
 
 class Artist(db.Model):
     __tablename__ = 'Artist'
