d835bf558259c703267d0419f7d71172c3913f26
Frank Nguyen <frank.nguyen.vd@gmail.com>
2021-03-13 22:39:41 +0800

    Add string representation for Artist

--- app.py
+++ app.py
@@ -60,6 +60,10 @@ class Artist(db.Model):
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
 
+    def __repr__(self):
+      return f'<Artist: {self.id}, {self.name}, {self.city}, {self.state}, {self.phone}, {self.genres}, {self.image_link}, {self.facebook_link}>'
+
+
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
