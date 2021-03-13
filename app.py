09c8b2d413d6e2977f7e4e3f4e3270970ecc9dbf
Frank Nguyen <frank.nguyen.vd@gmail.com>
2021-03-13 09:27:08 +0800

    Use port 80 to deploy website

--- app.py
+++ app.py
@@ -514,12 +514,11 @@ if not app.debug:
 #----------------------------------------------------------------------------#
 
 # Default port:
-if __name__ == '__main__':
-    app.run()
+# if __name__ == '__main__':
+#     app.run()
 
 # Or specify port manually:
-'''
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
-'''
+
+if __name__ == '__main__':    
+    app.run(host='0.0.0.0', port=80)
+
